124ae95dd01322c790f2f201b4ea21d351d13365
Francois Polo <andasan@gmail.com>
2020-05-20 16:32:01 -0700

    added answer

--- routes/members.js
+++ routes/members.js
@@ -2,24 +2,24 @@ const express = require('express');
 const router = express.Router();
 const uuid = require('uuid');
 
-const members = require('../model/Members');
+let members = require('../model/Members');
 
-router.get('/', (req, res)=> {
-    res.json(members);
+router.get('/', (req, res) => {
+  res.json(members);
 });
 
 router.get('/:id', (req, res) => {
   const paramsId = req.params.id;
   const found = members.some((member) => member.id === parseInt(paramsId));
 
-  if(found){
+  if (found) {
     res.json(members.filter(member => member.id === parseInt(paramsId)))
-  }else{
-    res.status(400).json({ msg : `member with ${paramsId} not found`});
+  } else {
+    res.status(400).json({ msg: `member with ${paramsId} not found` });
   }
 });
 
-router.post('/', (req, res)=> {
+router.post('/', (req, res) => {
 
   console.log("body: ", req.body);
   const newData = {
@@ -33,8 +33,33 @@ router.post('/', (req, res)=> {
 
 });
 
-router.put(); //update
+router.put('/:id', (req, res) => {
+  const paramsId = req.params.id;
+  const found = members.some((member) => member.id === parseInt(paramsId));
 
-router.delete(); //name explains itself
+  if (found) {
+    const index = members.findIndex(k => k.id == paramsId);
+    members[index] = {
+      ...members[index],
+      ...req.body
+    }
+    res.json(members);
+  } else {
+    res.status(400).json({ msg: `member with ${paramsId} not found` });
+  }
+})
+
+router.delete('/:id', (req, res) => {
+  const paramsId = req.params.id;
+  const found = members.some((member) => member.id === parseInt(paramsId));
+
+  if (found) {
+    const newList = members.filter(member => member.id !== parseInt(paramsId))
+    members = [...newList];
+    res.json(members);
+  } else {
+    res.status(400).json({ msg: `member with ${paramsId} not found` });
+  }
+})
 
 module.exports = router
